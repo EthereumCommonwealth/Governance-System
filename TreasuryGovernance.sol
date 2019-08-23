2e45a37484f53dd04c369fbad58d89ebb387c525
Dexaran <dexaran820@gmail.com>
2019-08-23 18:19:38 +0400

    Proxy proposal submission interface added for UI simplicity reasons.

--- TreasuryGovernance.sol
+++ TreasuryGovernance.sol
@@ -50,6 +50,21 @@ contract ColdStaking {
     
 }
 
+contract GovernanceProxy {
+    TreasuryGovernance public governance_contract = TreasuryGovernance(msg.sender);
+    address public owner = msg.sender;
+    function set_new_governance(address _governance)
+    {
+        require (msg.sender == owner);
+        governance_contract = TreasuryGovernance(_governance);
+    }
+    
+    function submit_new_proposal(string _name, string _url, bytes32 _hash, uint _length_in_epochs, address _destination, uint _funding) public payable
+    {
+        governance_contract.submit_proposal(_name, _url, _hash, governance_contract.get_current_epoch() + 1, governance_contract.get_current_epoch() + 1 + _length_in_epochs, _destination, _funding);
+    }
+}
+
 contract TreasuryGovernance {
     
     // TODO: Update calculations with SafeMath functions.
